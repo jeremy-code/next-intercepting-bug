c7280c76e21719dafcd2bc6c98e44d10c492364c
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2024-01-03 14:23:40 -0800

    add app/default.tsx

---
+++ src/app/default.tsx
@@ -0,0 +1,13 @@
+const Default = () => {
+  console.log(
+    `The default component in app/default.tsx is being rendered with props`
+  );
+
+  return (
+    <div>
+      This is <code>default.tsx</code> and NOT the home page.
+    </div>
+  );
+};
+
+export default Default;
